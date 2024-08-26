239f7a9161f6efeab46c41550fadd41eff0ffea5
billychen0894 <billychen0894@gmail.com>
2024-08-25 17:27:36 -0700

    add: custom management command for testing QueueManager class

---
+++ task_manager/management/commands/test_queue.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from task_manager.queue_manager import QueueManager
+from task_manager.models import Task
+
+
+class Command(BaseCommand):
+    help = "Test task queue submission"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--host", type=str, help="RabbitMQ host")
+        parser.add_argument("--port", type=int, help="RabbitMQ port")
+
+    def handle(self, *args, **options):
+        # Create a task and add it to the queue
+        task = Task.objects.create(title="Test Task", description="This is a test task")
+
+        queue_manager = QueueManager(host=options["host"], port=options["port"])
+        queue_manager.connect()
+        queue_manager.submit_task(task)
+        queue_manager.close()
+
+        self.stdout.write(
+            self.style.SUCCESS(f"Successfully submitted task {task.id} to the queue")
+        )
