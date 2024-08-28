47a7f2f00d00f7da09e2d9d3ceebe12c81a95677
billychen0894 <billychen0894@gmail.com>
2024-08-27 19:13:57 -0700

    Update: modify TaskCreateView's custom behaviour to submit tasks to queue

--- task_manager/views.py
+++ task_manager/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Task
 from .serializers import TaskSerializer
+from .queue_manager import QueueManager
 
 
 class TaskCreateView(generics.CreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    # This method is called when a new task is created (overridden default behaviour from CreateAPIView)
+    def perform_create(self, serializer):
+        task = serializer.save()
+        queue_manager = QueueManager()
+        queue_manager.submit_task(task)
+        queue_manager.close()
