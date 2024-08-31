f7bc1610c4ceea6ae3f2e618eb7e91c5c3d87822
billychen0894 <billychen0894@gmail.com>
2024-08-30 19:01:55 -0700

    Add: TaskView for create, list, and view details

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,17 +1,48 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import status, viewsets
 from .models import Task
 from .serializers import TaskSerializer
 from .queue_manager import QueueManager
+from rest_framework.response import Response
+from django.urls import reverse
+from django.db import transaction
 
 
-class TaskCreateView(generics.CreateAPIView):
+class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-    # This method is called when a new task is created (overridden default behaviour from CreateAPIView)
-    def perform_create(self, serializer):
-        task = serializer.save()
+    @transaction.atomic
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         queue_manager = QueueManager()
-        queue_manager.submit_task(task)
-        queue_manager.close()
+        try:
+            with transaction.atomic():
+                task = serializer.save(status=Task.STATUS_PENDING)
+                queue_manager.submit_task(task)
+
+                task.status = Task.STATUS_QUEUED
+                task.save()
+
+                headers = self.get_success_headers(serializer.data)
+                if not headers and task.id:
+                    location = reverse("task-detail", kwargs={"pk": str(task.id)})
+                    headers["Location"] = request.build_absolute_uri(location)
+
+                return Response(
+                    serializer.data, status=status.HTTP_201_CREATED, headers=headers
+                )
+        except Exception as e:
+            return Response(
+                {"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        finally:
+            queue_manager.close()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = serializer.data
+        data["result"] = instance.get_result()
+        return Response(data)
