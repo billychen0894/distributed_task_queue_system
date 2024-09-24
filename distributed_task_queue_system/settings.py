4ae30ec23905746ba1de56329bc66483328017a7
billychen0894 <billychen0894@gmail.com>
2024-09-23 21:10:39 -0700

    Chore: remove prometheus middlewares

--- distributed_task_queue_system/settings.py
+++ distributed_task_queue_system/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django_prometheus.middleware.PrometheusBeforeMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -62,7 +61,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 ROOT_URLCONF = "distributed_task_queue_system.urls"
